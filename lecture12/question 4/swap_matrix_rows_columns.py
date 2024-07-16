615cb26cc4c8d82e8ae32b63406d7d3fd9267565
Alexus Huang <alexushu2325@gmail.com>
2024-07-15 23:37:50 -0700

    completed question 4 on chapter 12

--- lecture12/question 4/swap_matrix_rows_columns.py
+++ lecture12/question 4/swap_matrix_rows_columns.py
@@ -1,7 +1,12 @@
 import numpy as np
 
-M = np.array([1,2,3,4])
-def swap_rows(M,a,b):
-    pass
-def swap_columns(M,a,b):
-    pass
+M = np.array([[1,2,3],[4,5,6]])
+def swap_rows(array,a,b):
+    array[[a,b]] = array[[b,a]]
+    print(array)
+def swap_columns(array,a,b):
+    array[:,[a,b]] = array[:,[b,a]]
+    print(array)
+
+swap_rows(M,0,1)
+swap_columns(M,0,2)
