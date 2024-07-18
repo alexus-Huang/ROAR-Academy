8255917772c409e4d305db5a31f4e9f7147c4a19
Alexus Huang <alexushu2325@gmail.com>
2024-07-18 11:31:03 -0700

    update

--- samples/cartpole_dqn.py
+++ samples/cartpole_dqn.py
@@ -69,25 +69,25 @@ class DQNAgent:
 
 
 if __name__ == "__main__":
-    env = gym.make('CartPole-v1')
+    env = gym.make('CartPole-v1') # Create environment
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
-    agent = DQNAgent(state_size, action_size)
+    agent = DQNAgent(state_size, action_size) # DQN agent only needs to know the dimensions of the state and actions, the game play will be learned via observing rewards
     # agent.load("./save/cartpole-dqn.h5")
     done = False
-    batch_size = 32
+    batch_size = 32 #Update DQN every 32 steps
 
     for e in range(EPISODES):
         state = env.reset()
         state = np.reshape(state, [1, state_size])
         for time in range(500):
             # env.render()
-            action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
+            action = agent.act(state) # agent takes action based on state
+            next_state, reward, done, _ = env.step(action) # environment updates state and reward
             env.render()
             reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, state_size])
-            agent.remember(state, action, reward, next_state, done)
+            agent.remember(state, action, reward, next_state, done) # agent remembers state,action,reward,next_state as DQN's training sample
             state = next_state
             if done:
                 print("episode: {}/{}, score: {}, e: {:.2}"
